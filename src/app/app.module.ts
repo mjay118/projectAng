05c63bf5ef639ce537c0cc07c32d74c70aebbd48
mri118 <mrityunjaysinha0118@gmail.com>
2021-02-15 12:48:48 +0530

    msg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
@@ -17,6 +17,7 @@ import { AssignmentComponent } from './assignment/assignment.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/assignment/assignment.component.ts
+++ src/app/assignment/assignment.component.ts
@@ -7,37 +7,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignmentComponent implements OnInit {
 
-  employees=[
-    {
-      id:1,
-      name:'Mrityunjay',
-      age:20,
-      roles:['Developer'],
-    },
-    {
-      id:2,
-      name:'Ajay',
-      age:29,
-      roles:['HR'],
-    },
-    {
-      id:3,
-      name:'Ram',
-      age:25,
-      roles:['Admin'],
-    },
-    {
-      id:4,
-      name:'Shravya',
-      age:26,
-      roles:['Admin','HR'],
-    },
-  ]
+  // employees=[
+  //   {
+  //     id:1,
+  //     name:'Mrityunjay',
+  //     age:20,
+  //     roles:['Developer'],
+  //   },
+  //   {
+  //     id:2,
+  //     name:'Ajay',
+  //     age:29,
+  //     roles:['HR'],
+  //   },
+  //   {
+  //     id:3,
+  //     name:'Ram',
+  //     age:25,
+  //     roles:['Admin'],
+  //   },
+  //   {
+  //     id:4,
+  //     name:'Shravya',
+  //     age:26,
+  //     roles:['Admin','HR'],
+  //   },
+  //   {
+  //     id:5,
+  //     name:'Shravya',
+  //     age:26,
+  //     roles:['HR'],
+  //   },
+  // ]
+
+ename='';
+eage='';
+eroles='';
+  employees:any = [];
+
+
+
   
-  
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  addEmployee() {
+    
+    let empObj:any = {
+      name: this.ename,
+      age: this.eage,
+      roles: this.eroles,
+      id: this.employees.length + 1,
+    };
+    
+     this.employees.push(empObj);
 
+  }
 }
